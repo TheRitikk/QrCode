4e4f013de63dd887ece6ea20b48f5af9bb16ca45
Ritik Singh <56690279+TheRitikk@users.noreply.github.com>
2023-03-06 11:54:25 +0530

    Update

--- QrCode.py
+++ QrCode.py
@@ -1,8 +1,11 @@
 # import the qrcode module
 import qrcode as qr
 
+# input for qr
+qr_text = input("Enter Text/Link to generate the QR Code : ")
 # create qrcode using make function 
-img = qr.make("Hello Friends, Use this to create the QrCode")
+img = qr.make(qr_text)
 
 # save the qrcode
 img.save("video_Youtube.png")
+img.show()
